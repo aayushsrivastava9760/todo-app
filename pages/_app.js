83d9da5a9026f94e30b772db5ab29f0867580005
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2021-12-19 19:33:43 +0530

    updated ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../context/auth'
 import Nav from '../components/Nav'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <title>ToDo App</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </AuthProvider>
